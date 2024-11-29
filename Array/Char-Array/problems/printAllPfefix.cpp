c6a337d646d72e9f7609ddc186d684b66b3a479f
manish tomar <tomarmanish32@gmail.com>
2024-11-29 20:50:46 +0530

    from udemy

---
+++ Array/Char-Array/problems/printAllPfefix.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void printPrifix(string s)
+{
+    // BF
+    //  for (int i = 0; i < s.size(); i++)
+    //  {
+    //      for (int j = 0; j < i+1; j++)
+    //      {
+    //          cout << s.at(j);
+    //      }
+    //      cout << endl;
+    //  }
+
+    // TWO POINTER
+    //  int i =0;
+    //  int j =0;
+
+    // while(i<s.size()){
+    //     if (j<=i){
+    //         cout << s.at(j);
+    //     }
+    //     if(j==i){
+    //         i++;
+    //         j=0;
+    //         cout << endl;
+    //         continue;
+    //     }
+    //     j++;
+    // }
+
+    // SINGLE FOR LOOP
+    string str = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        str += s.at(i);
+        cout << str << endl;
+    }
+}
+
+int main()
+{
+    string s = "abcdefg";
+
+    printPrifix(s);
+    return 0;
+}
