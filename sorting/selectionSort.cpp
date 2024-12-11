a8bc75b778e8b184ce2fe2144529221de6429eb4
manish tomar <tomarmanish32@gmail.com>
2024-12-12 01:48:54 +0530

    added

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -16,22 +16,19 @@ void selectionSort(vector<int> &vec)
 {
     int smallest;
     int pos = 0;
-    int count =0;
     for (int i = 0; i < vec.size(); i++)
     {
         smallest = vec[i];
         pos=0;
-        count =0;
         for (int j = i; j < vec.size(); j++)
         {
             if (vec[j] < smallest)// if want to sort in des order change < to >
             {
                 smallest = vec[j];
                 pos = j;
-                count ++;
             }
         }
-        if(count>0){
+        if(smallest<vec[i]){
         swap(vec[i], vec[pos]);
         }
         display(vec);
@@ -40,7 +37,7 @@ void selectionSort(vector<int> &vec)
 
 int main()
 {
-    vector<int> vec = {7, 5, 3, 1, 6, 8,2};
+    vector<int> vec = {8,5,10,2,3,11,18,9};
 
     selectionSort(vec);
     return 0;
