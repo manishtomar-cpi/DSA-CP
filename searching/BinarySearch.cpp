355a7c2e04c8e918f11b4ccbda210e23c3183b68
manish tomar <tomarmanish32@gmail.com>
2024-12-13 00:15:16 +0530

    added

---
+++ searching/BinarySearch.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void display(vector<int> &vec)
+{
+    cout << "[ ";
+    for (auto &i : vec)
+    {
+        cout << i << " ";
+    }
+    cout << "]" << endl;
+}
+
+void sorting(vector<int> &vec)
+{
+
+    //? bubble sort
+    bool flag = false;
+
+    for (int i = 1; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec.size() - 1; j++)
+        {
+            if (vec[j + 1] < vec[j])
+            {
+                swap(vec[j + 1], vec[j]);
+                flag = true;
+            }
+        }
+        if (!flag)
+        {
+            break;
+        }
+    }
+    display(vec);
+}
+
+int binarySearch(vector<int> &vec, int key)
+{
+    int start = 0;
+    int end = vec.size() - 1;
+    int mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+        if (vec[mid] == key)
+        {
+            return mid;
+            break;
+        }
+        if (vec[mid] > key)
+        {
+            end = mid - 1;
+            mid = (start + end) / 2;
+        }
+        if (vec[mid] < key)
+        {
+            start = mid + 1;
+            mid = (start + end) / 2;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> vec{1, 2, 3, 4, 5,0,9,7,12,10};
+
+    sorting(vec);
+    cout << binarySearch(vec, 7) << endl;
+
+    return 0;
+}
