e2eca6a9e750abd9736ed7b2786a324a96279ee1
manish tomar <tomarmanish32@gmail.com>
2025-01-20 01:33:30 +0530

    added

---
+++ tree/Binary Tree/operations/minValue.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+class BTNode
+{
+private:
+    BTNode *root;
+    BTNode *left;
+    BTNode *right;
+    int data;
+
+    void inOrder(BTNode *root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        inOrder(root->left);
+        cout << root->data << ", ";
+        inOrder(root->right);
+    }
+
+public:
+    BTNode(int val) : data(val), root(nullptr), left(nullptr), right(nullptr) {};
+    BTNode() : root(nullptr), left(nullptr), right(nullptr) {};
+
+    void create()
+    {
+        cout << "Enter root data: ";
+        int rootData;
+        cin >> rootData;
+        cout << endl;
+        root = new BTNode(rootData);
+        queue<BTNode *> childs;
+        childs.push(root);
+
+        while (!childs.empty())
+        {
+            BTNode *current = childs.front();
+            childs.pop();
+            cout << "Enter left child of " << current->data << ": ";
+            int left;
+            cin >> left;
+            cout << endl;
+            if (left != -1) //-1 means no child
+            {
+                // create
+                BTNode *leftNode = new BTNode(left);
+                // making connection
+                current->left = leftNode;
+                // push into queue
+                childs.push(leftNode);
+            }
+            if (left == -1)
+            {
+                cout << "No left child of: " << current->data << endl;
+            }
+            cout << "Enter right child of " << current->data << ": ";
+            int right;
+            cin >> right;
+            cout << endl;
+            if (right != -1)
+            {
+                // create
+                BTNode *rightNode = new BTNode(right);
+                // making connection
+                current->right = rightNode;
+                // push into queue
+                childs.push(rightNode);
+            }
+            if (right == -1)
+            {
+                cout << "No right child of " << current->data << endl;
+            }
+        }
+    }
+
+    void print()
+    {
+        queue<BTNode *> childs;
+        if (root == nullptr)
+        {
+            return;
+        }
+        childs.push(root);
+
+        while (!childs.empty())
+        {
+            BTNode *current = childs.front();
+            childs.pop();
+            cout << current->data << ": ";
+            if (current->left)
+            {
+                childs.push(current->left);
+                cout << "L" << current->left->data << ", ";
+            }
+            if (current->right)
+            {
+                childs.push(current->right);
+                cout << "R" << current->right->data << ", ";
+            }
+            cout << endl;
+        }
+    }
+
+    int minVal()
+    {
+        if (root == nullptr)
+        {
+            return -1;
+        }
+        queue<BTNode *> childs;
+        childs.push(root);
+        int minVal = INT16_MAX;
+        while (!childs.empty())
+        {
+            BTNode *current = childs.front();
+            childs.pop();
+            if (current->data < minVal)
+            {
+                minVal = current->data;
+            }
+            if (current->left)
+            {
+                childs.push(current->left);
+            }
+            if (current->right)
+            {
+                childs.push(current->right);
+            }
+        }
+        return minVal;
+    }
+};
+
+int main()
+{
+    BTNode bt1;
+    bt1.create();
+    bt1.print();
+    cout << boolalpha;
+    cout << bt1.minVal() << endl;
+    return 0;
+}
